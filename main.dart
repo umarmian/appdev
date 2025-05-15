7610403655145dda01b720d374196de2859b49a0
MR UMAR FAROOQ <uufm098765@gmail.com>
2025-05-15 21:34:25 +0500

    mid term Q 3.2

--- main.dart
+++ main.dart
@@ -1,78 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:sqflite/sqflite.dart';
-import 'package:path/path.dart';
+import 'home.dart';
 
-void main() =>
-    runApp(MaterialApp(home: MyApp(), debugShowCheckedModeBanner: false));
-
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
+void main() {
+  runApp(const MyApp());
 }
 
-class _MyAppState extends State<MyApp> {
-  TextEditingController ctrl = TextEditingController();
-  List<String> items = [];
-  late Database db;
-
-  @override
-  void initState() {
-    super.initState();
-    initDb();
-  }
-
-  Future<void> initDb() async {
-    String path = join(await getDatabasesPath(), 'my.db');
-    db = await openDatabase(
-      path,
-      version: 1,
-      onCreate:
-          (db, v) => db.execute(
-            'CREATE TABLE data (id INTEGER PRIMARY KEY, text TEXT)',
-          ),
-    );
-    fetchData();
-  }
-
-  Future<void> save(String text) async {
-    if (text.isEmpty) return;
-    await db.insert('data', {'text': text});
-    ctrl.clear();
-    fetchData();
-  }
-
-  Future<void> fetchData() async {
-    final data = await db.query('data');
-    setState(() {
-      items = data.map((e) => e['text'].toString()).toList();
-    });
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Save Text to SQLite')),
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          children: [
-            TextField(
-              controller: ctrl,
-              decoration: InputDecoration(hintText: 'Enter text'),
-            ),
-            ElevatedButton(
-              onPressed: () => save(ctrl.text),
-              child: Text('Save'),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: items.length,
-                itemBuilder: (_, i) => ListTile(title: Text(items[i])),
-              ),
-            ),
-          ],
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(), // No 'const' here
     );
   }
 }
